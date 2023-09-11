77b5c85a91f6c5411d90db25379a9b122cacc4a9
TruongHocHuu <truonghochuuken@gmail.com>
2023-09-11 19:42:07 +0700

    Truong Hoc Huu

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Truong Hoc Huu</title>
+</head>
+<body>
+    <?php 
+    echo "Truong Hoc Huu";
+    $yearofbirth = 2004;
+    $thisyear = 2023;
+    $birthday = $thisyear - $yearofbirth;
+    echo $birthday;
+    ?>
+</body>
+</html>
+
+
+
+
+
